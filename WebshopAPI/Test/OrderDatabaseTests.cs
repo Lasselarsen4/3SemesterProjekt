15aefd5ce2b07b1d4b60da8459687b5aff925223
Lasselarsen4 <10478730@ucn.dk>
2024-05-14 10:38:33 +0200

    Update OrderDatabaseTests.cs

--- WebshopAPI/Test/OrderDatabaseTests.cs
+++ WebshopAPI/Test/OrderDatabaseTests.cs
@@ -22,8 +22,34 @@ namespace WebshopAPI.Test
 
 
         }
+        [Fact]
+        public void GetById_ReturnsOrderWithValidId()
+        {
+            // Arrange
+            var dbConnection = new DatabaseConnection();
+            var orderDatabase = new OrderDB(dbConnection);
+            var orderId = 1; 
 
-        // BASICALLY TESTER VI PÅ OM DER OVERHOVEDET KAN HENTES EN ORDER FRA DATABASEN
-       
+            // Act
+            var order = orderDatabase.GetById(orderId);
+
+            // Assert
+            Assert.NotNull(order);
+            Assert.Equal(orderId, order.OrderId);
+        }
+        [Fact]
+        public void GetById_ReturnsNullWithInvalidId()
+        {
+            // Arrange
+            var dbConnection = new DatabaseConnection(); 
+            var orderDatabase = new OrderDB(dbConnection);
+            var invalidOrderId = -1; 
+
+            // Act
+            var order = orderDatabase.GetById(invalidOrderId);
+
+            // Assert
+            Assert.Null(order);
+        }
     }
 }
