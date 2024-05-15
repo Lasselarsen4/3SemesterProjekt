b21346bf9e62b7e5111780878fff4cfd4a87a850
Lasselarsen4 <10478730@ucn.dk>
2024-05-15 10:32:07 +0200

    ServiceConnection

---
+++ WebshopApplication/ServiceLayer/IServiceConnection.cs
@@ -0,0 +1,13 @@
+namespace WebshopApplication.ServiceLayer {
+    public interface IServiceConnection {
+
+        public string? BaseUrl { get; init; }
+        public string? UseUrl { get; set; }
+
+        Task<HttpResponseMessage?> CallServiceGet();
+        Task<HttpResponseMessage?> CallServicePost(StringContent postJson);
+        Task<HttpResponseMessage?> CallServicePut(StringContent postJson);
+        Task<HttpResponseMessage?> CallServiceDelete();
+
+    }
+}
